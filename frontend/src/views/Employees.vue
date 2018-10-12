78c14ba0ff9fd8726e9b2c0b4d48613af43b148f
rg3915 <rg3915@yahoo.com.br>
2018-10-12 16:43:30 -0300

    Chamada http com axios

--- frontend/src/views/Employees.vue
+++ frontend/src/views/Employees.vue
@@ -1,5 +1,27 @@
 <template>
-    <div>
-        Employees
-    </div>
+  <div>
+    Employees
+    
+    <b-table striped hover :items="items"></b-table>
+
+  </div>
 </template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      items: []
+    }
+  },
+  created(){
+    axios.get('http://localhost:8000/api/crm/employee/')
+    .then(items => {
+      this.items = items.data
+    })
+  }
+}
+</script>
