32c93b8959d61a0c0e9ec642088b00e53cf69cb7
rg3915 <rg3915@yahoo.com.br>
2018-10-12 15:31:46 -0300

    Bootstrap-vue and Navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Navbar/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <router-view/>
@@ -8,11 +9,13 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navbar
   }
 }
 </script>
@@ -24,6 +27,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
 }
 </style>
