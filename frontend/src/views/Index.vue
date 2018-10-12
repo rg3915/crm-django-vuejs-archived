d8cb813f6f0a69bc003759cf62970a47f5836ed0
rg3915 <rg3915@yahoo.com.br>
2018-10-12 16:06:46 -0300

    Implementou a store

--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -1,5 +1,25 @@
 <template>
-    <div>
-        Index
-    </div>
+  <div>
+    Index
+
+    Contador: {{ counter }}
+
+    <b-button variant="primary" @click="incrementStore"><i class="fa fa-plus"></i></b-button>
+  </div>
 </template>
+
+<script>
+  export default {
+    methods: {
+      incrementStore() {
+        this.$store.dispatch('increment')
+      }
+    },
+    computed: {
+      counter() {
+        return this.$store.state.counter
+      }
+    }
+  }
+
+</script>
