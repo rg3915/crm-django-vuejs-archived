77890534bf3c21731b009cd5b503d34912016a05
rg3915 <rg3915@yahoo.com.br>
2018-10-12 15:20:24 -0300

    vue-router

---
+++ frontend/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Index from '../views/Index.vue'
+import Employees from '../views/Employees.vue'
+
+Vue.use (Router)
+
+export default new Router ({
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: Index
+    },
+    {
+      path: '/crm/employees',
+      name: 'employees',
+      component: Employees
+    },
+  ]
+})
