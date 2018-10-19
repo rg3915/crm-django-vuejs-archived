1bfeb3996e99cebbfc3449776ea3936809793807
rg3915 <rg3915@yahoo.com.br>
2018-10-19 04:58:33 -0300

    Limpando views

--- backend/myproject/crm/views.py
+++ backend/myproject/crm/views.py
@@ -1,7 +1,4 @@
 from rest_framework import generics
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from .serializers import EmployeeSerializer
 from .models import Employee
 
