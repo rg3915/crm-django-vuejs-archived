372326c5cebadcc11456c5b5443faa190c275c7a
rg3915 <rg3915@yahoo.com.br>
2018-10-21 23:35:13 -0300

    Tentando usar DefaultRouter

--- backend/myproject/crm/urls.py
+++ backend/myproject/crm/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-# from rest_framework import routers
+from rest_framework import routers
 from myproject.crm import views as c
 
 app_name = 'crm'
 
-urlpatterns = [
-    path('employee/', c.EmployeeList.as_view()),
-    path('employee/<int:pk>/', c.EmployeeDetail.as_view()),
-]
+router = routers.DefaultRouter()
+router.register('employee/', c.Employee)
+
+# urlpatterns = [
+#     path('employee/', c.EmployeeList.as_view()),
+#     path('employee/<int:pk>/', c.EmployeeDetail.as_view()),
+# ]
--- backend/myproject/crm/views.py
+++ backend/myproject/crm/views.py
@@ -1,13 +1,20 @@
 from rest_framework import generics
+from rest_framework import mixins
+from rest_framework import viewsets
 from .serializers import EmployeeSerializer
 from .models import Employee
 
 
-class EmployeeList(generics.ListCreateAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
+# class EmployeeList(generics.ListCreateAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
 
 
-class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
+# class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
+
+
+class Employee(mixins.DestroyModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
